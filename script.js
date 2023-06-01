cdce9f35ba41e0ddae65a33193ac6a08d1c3bb70
PaulAlch <129964589+PaulAlch@users.noreply.github.com>
2023-06-01 18:02:34 +0200

    +
    +

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// Create WebGL iframe
-var unityIframe = document.createElement('iframe');
-unityIframe.src = 'index.html'; 
-unityIframe.width = '800';
-unityIframe.height = '600';
-document.body.appendChild(unityIframe);
-
-// Create screen share frame
-var screenShareFrame = document.createElement('iframe');
-screenShareFrame.width = '800';
-screenShareFrame.height = '600';
-document.body.appendChild(screenShareFrame);
-
-// Get screen share media stream
-async function startScreenShare() {
-  try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-    screenShareFrame.srcObject = mediaStream;
-  } catch (error) {
-    console.error('Error accessing screen share:', error);
-  }
+// Vérifier si le navigateur prend en charge le partage d'écran
+if (navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
+  // Obtenir l'accès au partage d'écran
+  navigator.mediaDevices.getDisplayMedia({ video: true })
+    .then(function(stream) {
+      // Obtenir la référence à la vidéo dans votre HTML
+      var videoElement = document.getElementById('screenShareVideo');
+      // Assigner le flux vidéo au lecteur vidéo
+      videoElement.srcObject = stream;
+      // Play la vidéo
+      videoElement.play();
+    })
+    .catch(function(error) {
+      console.error('Erreur lors du partage d\'écran :', error);
+    });
+} else {
+  console.error('Le navigateur ne prend pas en charge le partage d\'écran');
 }
-
-// Call the startScreenShare function when needed, e.g., on a button click event
-startScreenShare();
